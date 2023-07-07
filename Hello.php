a3ad59c1444c01f736501f1cd441be82ba8ec1ec
Jacob Shorte <jacob.shorte@gmail.com>
2023-07-07 17:17:39 +0100

    18. Create and access multidimensional arrays

--- Hello.php
+++ Hello.php
@@ -1,9 +1,10 @@
 <?php
 
 $articles = [
-     "One" => "First",
-      "Three" => "Second",
-       "Third"
-    ];
+    1 => ["title" => "First post", "content" => "This is the first post"],
+    3 => ["title" => "Second post", "content" => "This is the second post"],
+    5 => ["title" => "Final post", "context" => "This is the final post"]
+];
 
-var_dump($articles["Three"]);
+# var_dump($articles);
+var_dump($articles[3]["title"]);
