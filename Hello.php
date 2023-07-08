18411b17419a0fd3b8936af8eb17dd45b53046e7
Jacob Shorte <jacob.shorte@gmail.com>
2023-07-08 17:13:24 +0100

    27. The language of web pages: an introduction to HTML

--- Hello.php
+++ Hello.php
@@ -1,18 +1,7 @@
 <?php
 
-$day = "Sponge";
-
-switch($day)
-{
-    case "Mon":
-        echo "Monday";
-        break;
-    case "Tue":
-        echo "Tuesday";
-        break;
-    case "Wed":
-        echo "Wednesday";
-        break;
-    default:
-        echo "Not a day";
+if (false) {
+    echo "stop!";
+} else {
+    echo "go!";
 }
