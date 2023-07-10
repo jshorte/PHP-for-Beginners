70603f0c8b2136cf31e4c05544a3c878357d07e8
Jacob Shorte <jacob.shorte@gmail.com>
2023-07-10 20:37:27 +0100

    47. Combine PHP and HTML to show a formatted list of articles

--- index.php
+++ index.php
@@ -13,22 +13,57 @@ if(mysqli_connect_error()) {
     exit;
 }
 
-echo "Connection Established";
+# echo "Connection Established";
 
 //Get data from the article table
 $sql = "SELECT *
-        FROM article
-        ORDER BY published_at;";
+        FROM article        
+        ORDER BY id DESC;";
 
-        $results = mysqli_query($conn, $sql); //Query db
+$results = mysqli_query($conn, $sql); //Query db
 
-        //Print error
-        if($results === false) {
-            echo mysqli_error($conn);
-        } 
-        //Fetch and display results (Associative to display names for columns)
-        else {
-            $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+//Print error
+if($results === false) {
+    echo mysqli_error($conn);
+} 
+//Fetch and display results (Associative to display names for columns)
+else {
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
 
-            var_dump($articles);
-        }
+    # var_dump($articles);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My blog</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+    <header>
+        <h1>My blog</h1>
+    </header>
+
+    <main>
+        <?php if (empty($articles)): ?>
+            <p>No articles found.</p>
+        <?php else: ?>
+
+            <ul>                
+                <?php foreach ($articles as $article): ?>
+                    <li>
+                        <article>
+                            <h2><?= $article['title']; ?></h2>
+                            <p><?= $article['content']; ?></p>
+                        </article>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+
+        <?php endif; ?>
+    </main>
+</body>
+</html>
