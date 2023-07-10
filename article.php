cc3fd67e626792ecb77287299fcfcb0efe9b6f2f
Jacob Shorte <jacob.shorte@gmail.com>
2023-07-10 22:52:18 +0100

    50. Avoid SQL injection: Validate the ID passed in from the query string

--- article.php
+++ article.php
@@ -13,24 +13,27 @@ if(mysqli_connect_error()) {
     exit;
 }
 
-# echo "Connection Established";
+if (isset($_GET['id']) && is_numeric($_GET['id'])){ //Checks if URL param is a number and has been defined
 
-//Get data from the article table
-$sql = "SELECT *
-        FROM article        
-        WHERE id = " . $_GET['id']; //Gets the key from url param specified by index.php to access the associated value
+    //Get data from the article table
+    $sql = "SELECT *
+            FROM article        
+            WHERE id = " . $_GET['id']; //Gets the key from url param specified by index.php to access the associated value
 
-$results = mysqli_query($conn, $sql); //Query db
+    $results = mysqli_query($conn, $sql); //Query db
 
-//Print error
-if($results === false) {
-    echo mysqli_error($conn);
-} 
-//Fetch and display row (Associative to display names for columns)
-else {
-    $article = mysqli_fetch_assoc($results);
+    //Print error
+    if($results === false) {
+        echo mysqli_error($conn);
+    } 
+    //Fetch and display row (Associative to display names for columns)
+    else {
+        $article = mysqli_fetch_assoc($results);
+    }
 
-    # var_dump($articles);
+}
+else { //Null if not a number
+    $article = null;
 }
 
 ?>
