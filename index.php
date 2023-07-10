c13a72bb04becae99f288451cad228e414b832c5
Jacob Shorte <jacob.shorte@gmail.com>
2023-07-10 12:11:01 +0100

    35. Create dynamic content: Mixing HTML and PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "Jacob";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>    
+    <title>My Website</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>Header 1</h1>
+
+    <p>Hello, <?= $name; ?></p>    
+</body>    
