7a04578de6f5fca8176186477caa4ec151d5f623
Jacob Shorte <jacob.shorte@gmail.com>
2023-07-16 13:11:53 +0100

    83. Add a form for editing an exisiting article

--- edit-article.php
+++ edit-article.php
@@ -9,9 +9,29 @@ if (isset($_GET['id'])){ //Checks if URL param is a number and has been defined
 
     $article = getArticle($conn, $_GET['id']);
 
+    if( $article)
+    {
+        $title = $article['title'];
+        $content = $article['content'];
+        $published_at = $article['published_at'];
+    }
+    else
+    {
+        $die("article not found");
+    }
+
 }
-else { //Null if not a number
-    $article = null;
+else 
+{ //Null if not a number
+    $die("id not supplied, article not found");
 }
 
-var_dump($article);
+
+require 'includes/header.php' ?>
+
+<h2>Edit article</h2>
+
+<?php 
+    require 'includes/article-form.php';
+    require 'includes/footer.php';
+?>
--- new-article.php
+++ new-article.php
@@ -94,35 +94,7 @@ require 'includes/header.php' ?>
 
 <h2>New article</h2>
 
-<!-- Output errors into list -->
-<?php if (!empty($errors)): ?>
-    <ul>
-        <?php foreach($errors as $error): ?>
-            <li><?= $error ?></li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
-
-<form method="post">
-
-<!-- Display values if previously submitted -->
-<!-- "htmlspecialchars" convert special chars to html to avoid XSS injection -->
-    <div>
-        <label for="title">Title</label>
-        <input name="title" id="title" placeholder="Article title" value="<?= htmlspecialchars($title); ?>"> 
-    </div>
-
-    <div>
-        <textarea name="content" id="content" rows="4" cols="40" placeholder="Article Content"><?= htmlspecialchars($content); ?></textarea>
-    </div>
-
-    <div>
-        <label for="published_at">Publication date and time</label>
-        <input type="datetime-local" name="published_at" id="published_at" value="<?= htmlspecialchars($published_at); ?>">
-    </div>
-
-    <button>Add</button>
-
-</form>
-
-<?php require 'includes/footer.php' ?>
+<?php 
+    require 'includes/article-form.php';
+    require 'includes/footer.php';
+?>
